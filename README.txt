476b19bc5a559ab73901e36342784d9942f1e8ad
Derek Hoy <derek@snowcloud.co.uk>
2011-05-09 14:21:51 +0100

    some initial files.

---
+++ README.txt
@@ -0,0 +1,3 @@
+Simple Django blog app for ClinicalTemplates.org web framework.
+
+See http://github.com/snowcloud/ct-snowhite for example project.
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ct-blog',
+    version='0.1dev',
+    packages=['ct_blog',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
