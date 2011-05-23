2275928e8b044096f74199b96580dae1d85942c4
Derek Hoy <derek@snowcloud.co.uk>
2011-05-23 20:13:46 +0100

    added import for truncatewords.

--- ct_blog/models.py
+++ ct_blog/models.py
@@ -3,6 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.db.models import Manager, permalink
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.template.defaultfilters import truncatewords
 from django.template.loader import render_to_string
 
 import datetime
